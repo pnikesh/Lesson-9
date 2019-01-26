31d823941ba3ab6362c7e380fa465ed18d63e1de
Nik <nikeshpatel356@gmail.com>
2019-01-26 12:16:49 -0500

    Icon highligher - EJS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('index', { title: 'Contact' });
+});
+router.get('/products', (req, res, next) => {
+  res.render('index', { title: 'Products' });
+});
+
+router.get('/services', (req, res, next) => {
+  res.render('index', { title: 'Services' });
 });
 
 module.exports = router;
