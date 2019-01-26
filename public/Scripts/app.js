b75f8c710aa22fedb40ab268744aaaf397b5751f
Nik <nikeshpatel356@gmail.com>
2019-01-26 12:00:05 -0500

    Restructured project

---
+++ public/Scripts/app.js
@@ -0,0 +1,8 @@
+/*custom JavaScript goes here*/
+//IIFE
+(function(){
+    function Start(){
+            console.log(`%c App strted...`, "font-size: 20px; color: blue; font-weight:bold")
+    }
+    window.addEventListener("load",Start);
+})();
