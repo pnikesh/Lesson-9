87e28de4748a4b0e4ee8e33b3b61d0fd26c9cc5c
Nik <nikeshpatel356@gmail.com>
2019-02-02 12:22:39 -0500

    Published on mLab

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 //configuration for databse
 module.exports = {
     //local mongo db deployment
-    "URI":"mongodb://localhost/demo_mongo"
+    //"URI":"mongodb://localhost/demo_mongo"
+    "URI":"mongodb://nik:password123@ds058739.mlab.com:58739/emerge_mongo"
 }
